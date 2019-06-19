d82fa22085ecc30e4c25ddb0ad1c4c858f538682
Jose Colon Rodriguez <jec.rod@gmail.com>
2019-06-19 18:41:08 -0400

    Factored out cache into module. Moved server from dir to file

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod server;
+pub mod cache;
 
 #[cfg(test)]
 mod tests {
--- src/server.rs
+++ src/server.rs
@@ -1,27 +1,23 @@
-use std::collections::HashMap;
-use std::fs::File;
-use std::hash::BuildHasherDefault;
 use std::io;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
 use std::str;
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 use openssl::ssl::{AlpnError, Error, SslAcceptor, SslFiletype, SslMethod, SslStream};
-use seahash::SeaHasher;
 use solicit::http::connection::{EndStream, HttpConnection, SendStatus};
 use solicit::http::server::ServerConnection;
 use solicit::http::session::{DefaultSessionState, SessionState, Stream};
 use solicit::http::transport::TransportStream;
 use solicit::http::{Header, HttpScheme, Response, StreamId};
 
+use crate::cache;
+
 /// Server is a simple HTT/2 server
 pub struct Server {
     acceptor: Arc<SslAcceptor>,
     listener: TcpListener,
-    cache: Arc<MemCache>,
+    cache: Arc<cache::MemCache>,
 }
 
 impl Server {
@@ -30,7 +26,7 @@ impl Server {
         Ok(Server {
             acceptor: Server::new_acceptor(cert, key)?,
             listener: TcpListener::bind(socket)?,
-            cache: MemCache::new(),
+            cache: cache::MemCache::new(),
         })
     }
 
@@ -111,31 +107,8 @@ impl TransportStream for Wrapper {
     }
 }
 
-/// get_ctype produces a MIME content type string based on filename extension.
-pub fn get_ctype(filename: &str) -> &str {
-    let mut ctype = "application/octet-stream";
-
-    if let Some(dot) = filename.rfind('.') {
-        ctype = match &filename[dot..] {
-            ".html" | ".htm" => "text/html; charset=utf-8",
-            ".css" => "text/css",
-            ".js" => "text/javascript",
-            ".png" => "image/png",
-            ".jpg" | ".jpeg" => "image/jpeg",
-            ".gif" => "image/gif",
-            ".svg" => "image/svg+xml",
-            ".webp" => "image/webp",
-            ".txt" => "text/plain; charset=utf-8",
-            ".json" => "application/json",
-            _ => "binary/octet-stream",
-        }
-    }
-
-    &ctype
-}
-
 /// handle_request processes an HTTP/2 request. It always returns a Response.
-fn handle_request(req: ServerRequest, cache: Arc<MemCache>) -> Response {
+fn handle_request(req: ServerRequest, cache: Arc<cache::MemCache>) -> Response {
     let mut filename = String::from("index.html");
     for (name, value) in req.headers {
         let name = str::from_utf8(&name).unwrap();
@@ -155,7 +128,7 @@ fn handle_request(req: ServerRequest, cache: Arc<MemCache>) -> Response {
 }
 
 /// handle_stream processess an HTTP/2 TCP/TLS streaml
-fn handle_stream(stream: TcpStream, acceptor: Arc<SslAcceptor>, cache: Arc<MemCache>) {
+fn handle_stream(stream: TcpStream, acceptor: Arc<SslAcceptor>, cache: Arc<cache::MemCache>) {
     let stream = match acceptor.accept(stream) {
         Ok(stream) => stream,
         Err(e) => {
@@ -233,90 +206,5 @@ fn handle_stream(stream: TcpStream, acceptor: Arc<SslAcceptor>, cache: Arc<MemCa
             }
         }
         let _ = conn.state.get_closed();
-    }
-}
-
-/// BuildHasher lets us use SeaHasher with HashMap.
-type BuildHasher = BuildHasherDefault<SeaHasher>;
-
-// Memcache is a concurrency safe cache for Responses.
-struct MemCache {
-    store: RwLock<HashMap<String, Response, BuildHasher>>,
-}
-
-impl MemCache {
-    /// new returns a new initialized MemCache instance.
-    pub fn new() -> Arc<MemCache> {
-        Arc::new(MemCache {
-            store: RwLock::new(HashMap::<String, Response, BuildHasher>::default()),
-        })
-    }
-
-    /// get returns an HTTP/2 response for filename. It always returns a Response.
-    fn get(&self, filename: &String) -> Response {
-        // Short circuit return if found
-        if let Some(resp) = self.store.read().unwrap().get(filename) {
-            return resp.clone();
-        }
-
-        let file = match File::open(filename) {
-            Ok(file) => file,
-            Err(e) => {
-                eprintln!("error opening file {}: {}", filename, e);
-                if io::ErrorKind::NotFound == e.kind() {
-                    return Response {
-                        headers: vec![(b":status".to_vec(), b"404".to_vec())],
-                        body: b"Not Found\n".to_vec(),
-                        stream_id: 0,
-                    };
-                }
-                return Response {
-                    headers: vec![(b":status".to_vec(), b"500".to_vec())],
-                    body: b"Unable to get file\n".to_vec(),
-                    stream_id: 0,
-                };
-            }
-        };
-
-        let meta = match file.metadata() {
-            Ok(meta) => meta,
-            Err(e) => {
-                eprintln!("error reading file {} metadata: {}", filename, e);
-                return Response {
-                    headers: vec![(b":status".to_vec(), b"500".to_vec())],
-                    body: b"Unable to get file metadata\n".to_vec(),
-                    stream_id: 0,
-                };
-            }
-        };
-
-        let mut buf_reader = BufReader::new(file);
-        let mut buf = Vec::with_capacity(meta.len() as usize);
-        if let Err(e) = buf_reader.read_to_end(&mut buf) {
-            eprintln!("error reading file {}: {}", filename, e);
-            return Response {
-                headers: vec![(b":status".to_vec(), b"500".to_vec())],
-                body: b"Unable to read file\n".to_vec(),
-                stream_id: 0,
-            };
-        }
-
-        let ctype = get_ctype(filename);
-
-        let resp = Response {
-            headers: vec![
-                (b":status".to_vec(), b"200".to_vec()),
-                (b"content-type".to_vec(), ctype.as_bytes().to_vec()),
-            ],
-            body: buf,
-            stream_id: 0,
-        };
-
-        self.store
-            .write()
-            .unwrap()
-            .insert(filename.to_string(), resp.clone());
-
-        resp
     }
 }

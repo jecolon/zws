50e9731433f900842b6fedb8e06953faebe2a0c5
Jose Colon Rodriguez <jec.rod@gmail.com>
2019-06-30 22:54:30 -0400

    Added customizable StringHandler

--- src/bin/zws.rs
+++ src/bin/zws.rs
@@ -3,26 +3,30 @@ use zws::{Handler, Response, Server, ServerRequest};
 
 fn main() -> zws::Result<()> {
     Server::new("tls/dev/cert.pem", "tls/dev/key.pem", "127.0.0.1:8443")?
-        .add_handler("GET /hello", HelloHandler::new())?
-        .add_handler("GET /hello/world", HelloHandler::new())?
+        .add_handler("GET /hello", StringHandler::new("Hello"))?
+        .add_handler("GET /hello/world", StringHandler::new("World"))?
         .add_handler("GET /", StaticFile::new("webroot", true)?)?
         .run()
 }
 
-struct HelloHandler;
+struct StringHandler {
+    body: Vec<u8>,
+}
 
-impl HelloHandler {
-    fn new() -> Box<HelloHandler> {
-        Box::new(HelloHandler {})
+impl StringHandler {
+    fn new(s: &str) -> Box<StringHandler> {
+        Box::new(StringHandler {
+            body: s.as_bytes().to_vec(),
+        })
     }
 }
 
-impl Handler for HelloHandler {
+impl Handler for StringHandler {
     fn handle(&self, req: ServerRequest) -> Response {
         Response {
             stream_id: req.stream_id,
             headers: vec![(b":status".to_vec(), b"200".to_vec())],
-            body: b"Hello world!".to_vec(),
+            body: self.body.clone(),
         }
     }
 }
